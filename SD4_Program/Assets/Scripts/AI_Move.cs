fcbfd1b65dc030536950c07835d635bd85a52734
Jeremy Zoitas <jezz331@gmail.com>
2019-11-21 15:22:20 +1100

    Initial commit, completed the naive approach which is the slowest way to check for the nearest other coloured object, also added kdtree technique wich can be ticked to swap between the two methods, this demo includes a group of ai that walk forwards and spawn at the start of the level, it also can change the amount that spawns before playing.

---
+++ SD4_Program/Assets/Scripts/AI_Move.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AI_Move : MonoBehaviour
+{
+    public Transform m_walkTowardsPoint;
+    private NavMeshAgent m_agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3 (transform.position.x,transform.position.y, Random.Range(30, -30));
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_agent.destination = m_walkTowardsPoint.position;
+    }
+}
---
+++ SD4_Program/Assets/Scripts/FindClosest.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class FindClosest : MonoBehaviour
+{
+    public GameObject WhiteAIPrefab;
+    public GameObject BlackAIPrefab;
+    public int m_nCountBlack;
+    public int m_nCountWhite;
+    protected KdTree<AI_Move> m_WhiteAgents = new KdTree<AI_Move>();
+    protected KdTree<AI_Move> m_BlackAgents = new KdTree<AI_Move>();
+
+    public bool m_bKdTree = false;
+    public bool m_bOctree = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < m_nCountWhite; i++)
+        {
+            m_WhiteAgents.Add(Instantiate(WhiteAIPrefab).GetComponent<AI_Move>());
+        }
+        for (int i = 0; i < m_nCountBlack; i++)
+        {
+            m_BlackAgents.Add(Instantiate(BlackAIPrefab).GetComponent<AI_Move>());
+        }
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_bKdTree)
+        {
+            m_BlackAgents.UpdatePositions();
+
+            foreach (var m_WhiteAgent in m_WhiteAgents)
+            {
+                AI_Move nearestObject = m_BlackAgents.FindClosest(m_WhiteAgent.transform.position);
+
+                Debug.DrawLine(m_WhiteAgent.transform.position, nearestObject.transform.position, Color.red);
+            }
+        }
+        else if(m_bOctree)
+        {
+            
+        }
+        else if(!m_bKdTree && !m_bOctree)
+        {
+            foreach (var m_WhiteAgent in m_WhiteAgents)
+            {
+                var nearestDist = float.MaxValue;
+                AI_Move nearestObj = null;
+
+                foreach (var m_BlackAgent in m_BlackAgents)
+                {
+                    nearestDist = Vector3.Distance(m_WhiteAgent.transform.position, m_BlackAgent.transform.position);
+                    nearestObj = m_BlackAgent;
+                }
+                Debug.DrawLine(m_WhiteAgent.transform.position, nearestObj.transform.position, Color.red);
+            }
+        }
+    }
+}
