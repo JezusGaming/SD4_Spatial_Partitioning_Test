427076346986f018d8cc19ccd935a4c731c94020
Jeremy Zoitas <jezz331@gmail.com>
2019-11-27 16:29:44 +1100

    Attempted to add quadtrees

--- SD4_Program/Assets/Scripts/FindClosest.cs
+++ SD4_Program/Assets/Scripts/FindClosest.cs
@@ -9,22 +9,52 @@ public class FindClosest : MonoBehaviour
     public int m_nCountWhite;
     protected KdTree<AI_Move> m_WhiteAgents = new KdTree<AI_Move>();
     protected KdTree<AI_Move> m_BlackAgents = new KdTree<AI_Move>();
+    protected List<AI_Move> m_WhiteQuadAgent = new List<AI_Move>();
+    private List<TestObject> m_QuadAgentsWhite;
+    private List<TestObject> m_QuadAgentsBlack;
+
+    public Vector2 recPos;
+    public Vector2 recSize;
+    private Rect m_rect;
+
+    private TestObject newObject;
 
     public bool m_bKdTree = false;
     public bool m_bOctree = false;
 
+    public class TestObject : IQuadTreeObject
+    {
+        private Vector3 m_vPosition;
+        public TestObject(Vector3 position)
+        {
+            m_vPosition = position;
+        }
+        public Vector2 GetPosition()
+        {
+            //Ignore the Y position, Quad-trees operate on a 2D plane.
+            return new Vector2(m_vPosition.x, m_vPosition.z);
+        }
+    }
+    QuadTree<TestObject> quadTree;
+
     // Start is called before the first frame update
     void Start()
     {
+        quadTree = new QuadTree<TestObject>(10, new Rect(-1000, -1000, 2000, 2000));
         for (int i = 0; i < m_nCountWhite; i++)
         {
             m_WhiteAgents.Add(Instantiate(WhiteAIPrefab).GetComponent<AI_Move>());
+            newObject = new TestObject(m_WhiteAgents[i].transform.position);
+            quadTree.Insert(newObject);
         }
         for (int i = 0; i < m_nCountBlack; i++)
         {
             m_BlackAgents.Add(Instantiate(BlackAIPrefab).GetComponent<AI_Move>());
+            newObject = new TestObject(m_BlackAgents[i].transform.position);
+            quadTree.Insert(newObject);
         }
-	}
+        
+    }
 
     // Update is called once per frame
     void Update()
@@ -42,7 +72,41 @@ public class FindClosest : MonoBehaviour
         }
         else if(m_bOctree)
         {
-            
+            var shortestDistance = Mathf.Infinity;
+            GameObject whiteAgent;
+            GameObject BlackAgent;
+            foreach (var m_WhiteAgent in m_WhiteAgents)
+            {
+                
+
+                m_rect = new Rect(newObject.GetPosition(), recSize);
+
+                m_QuadAgentsWhite = quadTree.RetrieveObjectsInArea(m_rect);
+
+                for (int i = 0; i < m_QuadAgentsWhite.Count; i++)
+                {
+                    for (int j = 0; j < m_BlackAgents.Count; j++)
+                    {
+                        var distance = Vector2.Distance(m_QuadAgentsWhite[i].GetPosition(), m_BlackAgents[j].transform.position);
+                        if (distance < shortestDistance)
+                        {
+                            shortestDistance = distance;
+                            whiteAgent = m_WhiteAgent.gameObject;
+                            BlackAgent = m_BlackAgents[j].gameObject;
+                            //quadTree.Clear();
+                            Debug.DrawLine(whiteAgent.transform.position, BlackAgent.transform.position, Color.red);
+                        }
+                    }
+                }
+            }
+
+           
+
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    TestObject newObject = new TestObject(new Vector3(Random.Range(-900, 900), 0, Random.Range(-900, 900)));
+            //    quadTree.Insert(newObject);
+            //}
         }
         else if(!m_bKdTree && !m_bOctree)
         {
@@ -58,6 +122,14 @@ public class FindClosest : MonoBehaviour
                 }
                 Debug.DrawLine(m_WhiteAgent.transform.position, nearestObj.transform.position, Color.red);
             }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        if (quadTree != null)
+        {
+            quadTree.DrawDebug();
         }
     }
 }
